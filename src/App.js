7cc938801a9acc078f109ee2a2baac03f76ebf5e
rowendev <rowendevtw@gmail.com>
2022-10-17 22:13:24 +0800

    change structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Text1 from "./library/text-template/Text1";
-import Text2 from "./library/text-template/Text2";
+import Navbar from "./layout/Navbar";
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/text-example-1" element={<Text1 />} />
-      </Routes>
-    </Router>
+    <>
+      <Navbar />
+    </>
   );
 }
 
---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Navbar() {
+  // navbar structure
+  //
+  // <header>
+  //     <a href="" className="logo">company name</a>
+  //     <nav>
+  //         <ul>
+  //             <li><a href=""></a></li>
+  //             <li><a href=""></a></li>
+  //             <li><a href=""></a></li>
+  //         </ul>
+  //     </nav>
+  // </header>
+  //
+
+  return <div></div>;
+}
+
+export default Navbar;
