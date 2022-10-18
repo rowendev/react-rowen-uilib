b58442842b7abd2600e3e66f37e68cc49bef9cc2
rowendev <rowendevtw@gmail.com>
2022-10-18 21:58:51 +0800

    navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import Menu from "../asset/menu.svg";
+import Exit from "../asset/exit.svg";
 function Navbar() {
   // navbar structure
   //
@@ -15,7 +16,39 @@ function Navbar() {
   // </header>
   //
 
-  return <div></div>;
+  return (
+    <div className="forest-hero">
+      <div className="wrapper no-padding">
+        <header>
+          <a href="" className="logo">
+            company name
+          </a>
+          <nav>
+            <img src={Menu} alt="open menu" className="menu" id="menu-btn" />
+            <ul className="nav" id="nav">
+              <li className="exit" id="exit-btn">
+                <a href="">
+                  <img src={Exit} alt="exit" />
+                </a>
+              </li>
+
+              <li>
+                <a href="">Cabins</a>
+              </li>
+              <li>
+                <a href="">FAQ</a>
+              </li>
+              <li>
+                <a href="" className="res">
+                  Reservation
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
